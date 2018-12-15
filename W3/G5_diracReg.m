a871f311afe95146ba9f128ce2589acd81af621e
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2018-12-15 13:56:40 +0100

    Delete G5_diracReg.m

--- W3/G5_diracReg.m
+++
@@ -1,7 +0,0 @@
-function y = G5_diracReg( x, epsilon )
-%  Dirac function of x
-%    sol_diracReg( x, epsilon ) Computes the derivative of the heaviside
-%    function of x with respect to x. Regularized based on epsilon.
-
-y = epsilon./(pi*(epsilon^2 + x.^2)); %TODO 19: Line to complete
-
