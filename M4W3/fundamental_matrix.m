10bc3d8bb5639693d3a714e28ce7d44d29ddbadf
Quim Comas Martínez <joaquim.comas@e-campus.uab.cat>
2019-01-12 12:50:17 +0100

    Denormalization F matrix

--- M4W3/fundamental_matrix.m
+++ M4W3/fundamental_matrix.m
@@ -1,34 +1,40 @@
 function [ F ] = fundamental_matrix(x1_test, x2_test)
 % Computation of the Fundamental Matrix with 8 points or more.
 %   Detailed explanation goes here
+
 numb=size(x1_test,2); 
+
 %normalization of the points by the function provided 
 
-[x1_norm, ~] = normalise2dpts(x1_test);
-[x2_norm, ~] = normalise2dpts(x2_test);
+[x1_norm, T1] = normalise2dpts(x1_test);
+[x2_norm, T2] = normalise2dpts(x2_test);
 
-
- bu=x1_norm(1,:);
 %Compute linear combination of the matches such as W*f=0
 W=[x1_norm(1,:)'.* x2_norm(1,:)', x1_norm(2,:)'.* x2_norm(1,:)',...
    x2_norm(1,:)', x1_norm(1,:)'.* x2_norm(2,:)',x2_norm(2,:)',... 
    x1_norm(1,:)', x1_norm(2,:)',ones(1,numb)'];
 
+% Compute SVD, and f will be the last column of V
+[~,~,V]=svd(W);
 
-%Compute SVD, and f will be the last column of V
-[U,S,V]=svd(W);
-%Compose F 
+% Compose F 
 F=[V(1,end),V(2,end),V(3,end);
    V(2,end),V(4,end),V(5,end);
    V(3,end),V(5,end),V(6,end)];
-%Compute again the SVD of F and force F to be of rank 2, so D has a zero in
+
+% Compute again the SVD of F and force F to be of rank 2, so D has a zero in
 
 [U,S,V]=svd(F);
 
-%the his last value
+% the his last value
 
 F=U*[S(1,1),0,0;
      0,S(2,2),0;
      0 , 0  , 0]*V';
+
+% Denormalization
+
+F= T2'*F*T1;
+
 end
 
