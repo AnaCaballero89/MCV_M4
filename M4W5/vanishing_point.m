7eef95845b4946c673d02ac2b2a966767e08ad71
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2019-01-27 14:35:31 +0100

    Delete vanishing_point.m

--- M4W5/vanishing_point.m
+++
@@ -1,9 +0,0 @@
-function [v1] = vanishing_point(xo1, xf1, xo2, xf2)
-    l1 = cross(xo1,xf1);    % cross product
-    l1 = l1/l1(end);        % normalize
-
-    l2 = cross(xo2,xf2);
-    l2 = l2/l2(end);
-
-    v1 = cross(l1,l2); 
-end
